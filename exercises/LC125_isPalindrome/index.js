3c3380f53baabe59d6a3571071da9e52e280fe41
Kai Tang <16kt16@queensu.ca>
2020-01-12 22:10:22 -0500

    update

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,18 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  const stringS = s.toLowerCase().replace(/[\W_]/g, "");
+  let left = 0;
+  let right = stringS.length - 1;
+
+  while (left < right) {
+    if (stringS[left] !== stringS[right]) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+}
 
 module.exports = isPalindrome;
--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -1,3 +1,9 @@
-const climbStairs = n => {};
+const climbStairs = n => {
+  if (n <= 2) {
+    return n;
+  } else {
+    return climbStairs(n - 1) + climbStairs(n - 2);
+  }
+};
 
 module.exports = climbStairs;
